4453450acd4ad4fb4e890e8ba80e4443d6bbad42
tomnor <tomasn@kth.se>
2015-04-04 22:19:10 +0200

    delete dead code in datautils.py

--- channelpack/datautils.py
+++ channelpack/datautils.py
@@ -12,67 +12,6 @@ Helper module for processing data arrays and such.
 
 import numpy as np
 
-def array_and(d, conditions):
-    """Produce a boolean array based on conditions
-
-    d: dict
-        numpy 1d arrays. All arrays of the same shape (length). Keys are
-        integers
-
-    conditions: list
-        list of string conditions. Like
-        ['d[12] == 2', 'd[1] > d[10]'].
-
-    The condition strings are stripped from leading and trailing white
-    space.
-
-    Return all elements True if conditions is an empty list.
-
-    """
-
-    conlist = [con.strip() for con in conditions]
-    # print conlist, "'and'"
-    a = np.ones(len(d[d.keys()[0]])) == 1.0 # Initial True array.
-    if not conditions:                      # Maybe empty.
-        return a
-    for con in conlist:
-        try:
-            b = eval(con)
-        except SyntaxError as se:
-            raise SyntaxError('Error at eval in ' + __file__ + ' array_and.')
-        a = np.logical_and(a, b)
-    return a
-
-def array_or(d, conditions):
-    """Produce a boolean array based on conditions
-
-    d: dict
-        numpy 1d arrays. All arrays of the same shape (length). Keys are
-        integers
-
-    conditions: list
-        A list of conditions. Like
-        ['d[12] == 2', 'd[1] > d[10]'].
-
-    The condition strings are stripped from leading and trailing white
-    space.
-
-    Return all elements True if conditions is an empty list.
-    """
-    conlist = [con.strip() for con in conditions]
-    # print conlist, "'or'"
-    a = np.ones(len(d[d.keys()[0]])) == 1.0 # True
-    if not conlist or not conditions:
-        return a                # All True if no conditions.
-    a = a == 0.0                # False
-    for con in conlist:
-        try:
-            b = eval(con)
-        except SyntaxError as se:
-            raise SyntaxError('Error at eval in ' + __file__ + ' array_or.')
-        a = np.logical_or(a, b)
-    return a
-
 def masked(a, b):
     """Return a numpy array with values from a where elements in b are
     not False. Populate with numpy.nan where b is False. When plotting,
@@ -86,36 +25,6 @@ def masked(a, b):
         n = np.array([None for i in range(len(a))])
         # a = a.astype(object)
     return np.where(b, a, n) # a if b is True, else n.
-
-def duration_boolBAK(b, dur, durtype):
-    """
-    Mask the parts in b being True but does not meet the duration
-    rules. Return an updated copy of b.
-
-    b: 1d array with True or False elements.
-
-    dur: int
-        The number of consecutive elements to be True.
-
-    durtype: str
-        'strict', 'min' or 'max' accepted.
-
-    """
-    assert durtype in ['strict', 'min', 'max'], durtype
-
-    slicelst = slicelist(b)
-    b2 = np.array(b)
-
-    dt = durtype
-    for sc in slicelst:
-        cnt = sc.stop - sc.start
-        if ((dt == 'strict' and dur != cnt) or
-           (dt == 'min' and dur > cnt) or
-           (dt == 'max' and dur < cnt)):
-                b2[sc] = False
-
-    return b2
-
 
 def duration_bool(b, rule, samplerate=None):
     """
@@ -151,84 +60,6 @@ def duration_bool(b, rule, samplerate=None):
 
     return b2
 
-
-# def startstop_bool(d, start_con, stop_con, start_andor='and', stop_andor='and'):
-def startstop_boolBAK(pack):
-    """Make a bool array based on start and stop conditions.
-
-    d: dict
-        numpy 1d arrays. All arrays of the same shape (length). Keys are
-        integers
-
-    start_and, start_or, stop_and, stop_or_: list
-        lists of conditions. Like
-        ['d[12] == 2', 'd[1] > d[10]'].
-
-    Conditions formatting are as with the array_or and array_and
-    functions. The strings are likely produced by a ChannelPack
-    instance.
-
-    If no start conditions are set, return all True.
-
-    If there is start conditions but no stop conditions, this is legal,
-    the True section will begin at first start and remain the rest of
-    the array. Likewise, if there is stop conditions but no start
-    condition, the returned array will be all True until the first stop
-    slice, and the rest of the array is set to False.
-
-    NOTE: This function does not work yet. IN WORK.
-    UPDATE: I think it does work now. Update the docstring.
-    """
-
-    d = pack.D
-    b = np.ones(len(d[d.keys()[0]])) == True
-
-    start_and = pack.conconf.conditions_list('start_and')
-    start_or = pack.conconf.conditions_list('start_or')
-    stop_and = pack.conconf.conditions_list('stop_and')
-    stop_or = pack.conconf.conditions_list('stop_or')
-
-    # Pre-check:
-    runflag = 'startstop'
-    if not start_and and not start_or and not stop_and and not stop_or:
-        return b
-    elif not start_and and not start_or:
-        runflag = 'stoponly'
-    elif not stop_and and not stop_or:
-        runflag = 'startonly'
-
-    # startb:
-    if runflag == 'stoponly':
-        startb = b == False     # All False (Dummy assignment).
-    elif not start_and and start_or:
-        startb = array_or(d, start_or)
-    elif start_and and not start_or:
-        startb = array_and(d, start_and)
-    elif start_and and start_or:
-        a1 = array_and(d, start_and)
-        a2 = array_or(d, start_or)
-        startb = np.logical_and(a1, a2)
-    else:
-        assert 0, 'Semantic error'
-
-    # stopb:
-    if runflag == 'startonly':
-        stopb = b == False      # All False (Dummy assignment).
-    elif not stop_and and stop_or:
-        stopb = array_or(d, stop_or)
-    elif stop_and and not stop_or:
-        stopb = array_and(d, stop_and)
-    elif stop_and and stop_or:
-        a1 = array_and(d, stop_and)
-        a2 = array_or(d, stop_or)
-        stopb = np.logical_and(a1, a2)
-    else:
-        assert 0, 'Semantic error'
-
-    stopextend = pack.conconf.get_stopextend()
-
-    return _startstop_bool(startb, stopb, runflag, stopextend)
-
 def startstop_bool(pack):
     """Make a bool array based on start and stop conditions.
 
@@ -244,13 +75,6 @@ def startstop_bool(pack):
     """
     b_TRUE = np.ones(pack.rec_cnt) == True
 
-    # # Depricate---------------------------------
-    # start_and = pack.conconf.conditions_list('start_and')
-    # start_or = pack.conconf.conditions_list('start_or')
-    # stop_and = pack.conconf.conditions_list('stop_and')
-    # stop_or = pack.conconf.conditions_list('stop_or')
-    # # ---------------------------------
-
     start_list = pack.conconf.conditions_list('startcond')
     stop_list = pack.conconf.conditions_list('stopcond')
 
@@ -263,22 +87,6 @@ def startstop_bool(pack):
     elif not stop_list:
         runflag = 'start_only'
 
-    # # Depricate---------------------------------
-    # # startb:
-    # if runflag == 'stoponly':
-    #     startb = b == False     # All False (Dummy assignment).
-    # elif not start_and and start_or:
-    #     startb = array_or(d, start_or)
-    # elif start_and and not start_or:
-    #     startb = array_and(d, start_and)
-    # elif start_and and start_or:
-    #     a1 = array_and(d, start_and)
-    #     a2 = array_or(d, start_or)
-    #     startb = np.logical_and(a1, a2)
-    # else:
-    #     assert 0, 'Semantic error'
-    # # ---------------------------------
-
     # startb:
     if runflag == 'stoponly':
         startb = b_TRUE == False     # All False (Dummy assignment).
@@ -286,22 +94,6 @@ def startstop_bool(pack):
         startb = b_TRUE
         for cond in start_list:
             startb = startb & pack._mask_array(cond)
-
-    # # Depricate---------------------------------
-    # # stopb:
-    # if runflag == 'startonly':
-    #     stopb = b == False      # All False (Dummy assignment).
-    # elif not stop_and and stop_or:
-    #     stopb = array_or(d, stop_or)
-    # elif stop_and and not stop_or:
-    #     stopb = array_and(d, stop_and)
-    # elif stop_and and stop_or:
-    #     a1 = array_and(d, stop_and)
-    #     a2 = array_or(d, stop_or)
-    #     stopb = np.logical_and(a1, a2)
-    # else:
-    #     assert 0, 'Semantic error'
-    # # ---------------------------------
 
     # stopb:
     if runflag == 'startonly':
