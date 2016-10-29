6bda6e62d549a5873719954ba49c68623745c1f7
tomnor <tomasn@posteo.net>
2016-10-29 18:59:55 +0200

    copyright year update

--- channelpack/__init__.py
+++ channelpack/__init__.py
@@ -1,6 +1,6 @@
 
     # This is channelpack. Load and filter acquisition data.
-    # Copyright (C) 2014, 2015 Tomas Nordin
+    # Copyright (C) 2014, 2015, 2016 Tomas Nordin
 
     # This program is free software: you can redistribute it and/or modify
     # it under the terms of the GNU General Public License as published by
