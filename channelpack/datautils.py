3a650bd1daa184a2475e5634115770745d3449cf
Tomas Nordin <tomasn@posteo.net>
2020-04-19 12:22:21 +0200

    Rewrote startstop_bool function in datautils.py
    The basis for the start and stop trigger functionality is now this one
    function in datautils.py. The function is a generator.
    
    New testfile for the datautils.py file -- test_datautils.py.

--- channelpack/datautils.py
+++ channelpack/datautils.py
@@ -31,112 +31,48 @@ def masked(a, b):
         return np.where(b, a, np.full(len(a), None))
 
 
-def startstop_bool(pack):
-    """Make a bool array based on start and stop conditions.
+def startstop_bool(startb, stopb):
+    """Generator yielding bool values.
 
-    pack:
-        pack.ChannelPack instance
+    True elements in startb are used as start triggers and true
+    elements in stopb are used as stop triggers.
 
-    If there is start conditions but no stop conditions, this is legal,
-    the True section will begin at first start and remain the rest of
-    the array. Likewise, if there is stop conditions but no start
-    condition, the returned array will be all True until the first stop
-    slice, and the rest of the array is set to False.
+    True elements in stopb dominates startb.
 
-    """
-    b_TRUE = np.ones(pack.rec_cnt) == True  # NOQA
+    Sequences are looped over with `zip`, so will be truncated to the
+    shortest sequence.
 
-    start_list = pack.conconf.conditions_list('startcond')
-    stop_list = pack.conconf.conditions_list('stopcond')
+    Parameters
+    ----------
+    startb, stopb : sequence
+        Elements are tested with `if el...`
 
-    # Pre-check:
-    runflag = 'startstop'
-    if not start_list and not stop_list:
-        return b_TRUE
-    elif not start_list:
-        runflag = 'stoponly'
-    elif not stop_list:
-        runflag = 'start_only'
+    Example
+    -------
+    One descend
 
-    # startb:
-    if runflag == 'stoponly':
-        # all False (dummy assignment)
-        startb = b_TRUE == False  # NOQA
-    else:
-        startb = b_TRUE
-        for cond in start_list:
-            startb = startb & pack._mask_array(cond)
-
-    # stopb:
-    if runflag == 'startonly':
-        # all False (dummy assignment)
-        stopb = b_TRUE == False  # NOQA
-    else:
-        stopb = b_TRUE
-        for cond in stop_list:
-            stopb = stopb & pack._mask_array(cond)
-
-    stopextend = pack.conconf.get_stopextend()
-
-    return _startstop_bool(startb, stopb, runflag, stopextend)
-
-
-def _startstop_bool(startb, stopb, runflag, stopextend):
-    """Return boolean array based on start and stop conditions.
-
-    startb, stopb: Numpy 1D arrays of the same length.
-        Boolean arrays for start and stop conditions being fullfilled or not.
+    height: 1 2 3 4 5 4 3 2 1
+    startb: F F F F T F F F F (height == 5)
+    stobb:  T F F F F F F F T (height == 1)
+    result: F F F F T T T T F
+    -> height:      5 4 3 2
 
     """
-    # All false at start
-    res = np.zeros(len(startb)) == True  # NOQA
 
-    start_slices = slicelist(startb)
-    stop_slices = slicelist(stopb)
-
-    # Special case when there is a start but no stop slice or vice versa:
-    # if start_slices and not stop_slices:
-    if runflag == 'startonly':
-        try:
-            start = start_slices[0]
-            # Make True from first start and rest of array.
-            res[start.start:] = True
-            return res
-        except IndexError:
-            # Only start specified but no start condition
-            # fullfilled. Return all False.
-            return res
-
-    elif runflag == 'stoponly':
-        try:
-            stop = stop_slices[0]
-            res[:stop.start + stopextend] = True  # Make True up to first stop.
-            return res
-        except IndexError:
-            # Only stop specified but no stop condition fullfilled
-            # Return all True
-            return res == False  # NOQA
-
-    stop = slice(0, 0)           # For first check
-    start = slice(0, 0)          # For a possibly empty list start_slices.
-    for start in start_slices:
-        if start.start < stop.start:
-            continue
-        for stop in stop_slices:
-            if stop.start > start.start:
-                res[start.start: stop.start + stopextend] = True
-                break           # Next start
+    started = False
+    for start, stop in zip(startb, stopb):
+        if stop:
+            if started:
+                started = False
+            yield False
+        elif start:
+            if not started:
+                started = True
+            yield True
+        elif started:
+            yield True
         else:
-            # On a given start slice, the entire list of stop slices was
-            # exhausted, none being later than the given start. It must mean
-            # that from this given start, the rest is to be True:
-            break
-
-    # There was no stop for the last start in loop
-    if start.start > stop.start:
-        res[start.start:] = True
-
-    return res
+            yield False
 
 
 def slicelist(b):
---
+++ tests/test_datautils.py
@@ -0,0 +1,107 @@
+from __future__ import print_function
+import unittest
+import sys
+import os
+
+import numpy as np
+
+pardir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.insert(0, pardir)
+
+import channelpack as cp
+from channelpack.datautils import startstop_bool
+
+print('Testing channelpack package:', cp)
+print('Testing with', unittest)
+print()
+
+
+class TestStartStopBool(unittest.TestCase):
+    """Test the startstop_bool function."""
+
+    def setUp(self):
+        self.t_height = (1, 2, 3, 4, 5, 4, 3, 2, 1, 2, 3, 4, 5, 4, 3, 2, 1)
+        self.a_height = np.array(self.t_height)
+        self.expected = (5, 4, 3, 2, 5, 4, 3, 2)
+
+    def test_with_numpy_arrays_as_args(self):
+
+        descends = np.array(tuple(startstop_bool(self.a_height == 5,
+                                                 self.a_height == 1)))
+        self.assertTrue(np.all(self.a_height[descends] == self.expected))
+
+    def test_with_lists_as_args(self):
+
+        startlist = [True if el == 5 else False for el in self.t_height]
+        stoplist = [True if el == 1 else False for el in self.t_height]
+
+        truefalse = list(startstop_bool(startlist, stoplist))
+
+        self.assertTrue(np.all(self.a_height[truefalse] == self.expected))
+
+    def test_with_empty_sequences(self):
+
+        count = 0
+        for truefalse in startstop_bool((), ()):
+            count += 1
+        self.assertEqual(count, 0)
+
+    def test_start_no_stop(self):
+
+        startb = (0, 1, 0, 0)
+        stopb = (0, 0, 0, 0)
+        expected = (0, 1, 1, 1)
+
+        for b, compare in zip(startstop_bool(startb, stopb), expected):
+            if b:
+                self.assertTrue(compare)
+            else:
+                self.assertFalse(compare)
+
+    def test_stop_no_start(self):
+
+        startb = (0, 0, 0, 0)
+        stopb = (0, 1, 0, 0)
+        expected = (0, 0, 0, 0)
+
+        for b, compare in zip(startstop_bool(startb, stopb), expected):
+            if b:
+                self.assertTrue(compare)
+            else:
+                self.assertFalse(compare)
+
+    def test_start_stop_same(self):
+
+        startb = (0, 0, 1, 0)
+        stopb = (0, 0, 1, 0)
+        expected = (0, 0, 0, 0)
+
+        for b, compare in zip(startstop_bool(startb, stopb), expected):
+            if b:
+                self.assertTrue(compare)
+            else:
+                self.assertFalse(compare)
+
+    def test_one_start_one_stop(self):
+
+        startb = (0, 1, 0, 0)
+        stopb = (0, 0, 0, 1)
+        expected = (0, 1, 1, 0)
+
+        for b, compare in zip(startstop_bool(startb, stopb), expected):
+            if b:
+                self.assertTrue(compare)
+            else:
+                self.assertFalse(compare)
+
+    def test_start_all_true_one_stop(self):
+
+        startb = (1, 1, 1, 1)
+        stopb = (0, 1, 0, 0)
+        expected = (1, 0, 1, 1)
+
+        for b, compare in zip(startstop_bool(startb, stopb), expected):
+            if b:
+                self.assertTrue(compare)
+            else:
+                self.assertFalse(compare)
