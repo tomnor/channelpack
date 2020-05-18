e3749a6db425e5e529d9b8ac29d6c3fb3de1d38b
Tomas Nordin <tomasn@posteo.net>
2020-05-18 09:36:01 +0200

    Factory function to get a pack from dbf
    dbfpack() is the factory function.
    
    Also a version of the reader with a names argument for selection of
    fields to read, dbfrecords().
    
    Remove old functions.

--- channelpack/dbf.py
+++ channelpack/dbf.py
@@ -1,15 +1,18 @@
 import struct
 import datetime
-import itertools
+from collections import namedtuple
 
 import numpy as np
 
+from .readtext import contextopen
+from .pack import ChannelPack
 
 # dbfreader based on Raymond Hettinger recipe
 # http://code.activestate.com/recipes/362715/
 
 # dbf spec
 # http://www.clicketyclick.dk/databases/xbase/format/dbf.html#DBF_STRUCT
+
 
 def dbfreader(f):
     """Returns an iterator over records in a Xbase DBF file.
@@ -60,8 +63,8 @@ def dbfreader(f):
                 y, m, d = int(value[:4]), int(value[4:6]), int(value[6:8])
                 value = datetime.date(y, m, d)
             elif typ == b'L':
-                value = ((value in b'YyTt' and 'T') or
-                         (value in b'NnFf' and 'F') or '?')
+                value = ((value in b'YyTt' and 'T')
+                         or (value in b'NnFf' and 'F') or '?')
             elif typ == b'F':    # Can this type not be null?
                 value = float(value)
             elif typ == b'C':
@@ -70,51 +73,125 @@ def dbfreader(f):
         yield result
 
 
-def dbf_asdict(fn, usecols=None, keystyle='ints'):
-    """Return data from dbf file fn as a dict.
+FI = namedtuple('FieldInfo', ('name', 'typ', 'size', 'deci',
+                              'fmt', 'fmtsiz', 'keep', 'seekme'))
 
-    fn: str
-        The filename string.
 
-    usecols: seqence
-        The columns to use, 0-based.
+def dbfrecords(f, names):
+    """Returns an iterator over records in a Xbase DBF file.
 
-    keystyle: str
-        'ints' or 'names' accepted. Should be 'ints' (default) when this
-        function is given to a ChannelPack as loadfunc. If 'names' is
-        used, keys will be the field names from the dbf file.
+    The first row returned contains the field names. The second row
+    contains field specs: (type, size, decimal places). Subsequent rows
+    contain the data records. If a record is marked as deleted, it is
+    skipped.
+
+    names is the field names to extract.
+
+    File should be opened for binary reads.
+
+    """
+    numrec, lenheader = struct.unpack('<xxxxLH22x', f.read(32))
+    numfields = (lenheader - 33) // 32
+
+    # discarded in main loop
+    fields = [FI('DeletionFlag', 'C', 1, 0,
+                 '1s', struct.calcsize('1s'), True, 0)]
+
+    for fieldno in range(numfields):
+        name, typ, size, deci = struct.unpack('<11sc4xBB14x', f.read(32))
+        name = name.replace(b'\0', b'')       # eliminate NULs from string
+        name = name.decode('ascii')
+        fmt = str(size) + 's'
+        prev = fields[fieldno]
+        fi = FI(name, typ, size, deci, fmt,
+                struct.calcsize(fmt), name in names, prev.seekme + prev.size)
+        fields.append(fi)
+
+    selfields = [field for field in fields if field.keep]
+    yield [field.name for field in selfields[1:]]
+    yield [tuple(field[1:4]) for field in selfields[1:]]
+
+    terminator = f.read(1)
+    assert terminator == b'\r'
+
+    for i in range(numrec):
+        refaddr = f.tell()
+        record = []
+        for field in selfields:
+            f.seek(refaddr + field.seekme)
+            record.append(struct.unpack(field.fmt, f.read(field.fmtsiz))[0])
+
+        if record[0] != b' ':
+            continue                        # deleted record
+        result = []
+        for sf, value in zip(selfields, record):
+            if sf.name == 'DeletionFlag':
+                continue
+            if sf.typ == b'N':
+                value = value.replace(b'\0', b'').lstrip()
+                if value == b'':
+                    value = np.nan
+                elif sf.deci:
+                    value = float(value)
+                else:
+                    value = int(value)
+            elif sf.typ == b'D':
+                y, m, d = int(value[:4]), int(value[4:6]), int(value[6:8])
+                value = datetime.date(y, m, d)
+            elif sf.typ == b'L':
+                value = ((value in 'YyTt' and 'T')
+                         or (value in 'NnFf' and 'F') or '?')
+            elif sf.typ == b'F':
+                value = float(value)
+            elif sf.typ == b'C':
+                value = value.decode('ascii')
+            result.append(value)
+        f.seek(refaddr + fields[-1].seekme + fields[-1].fmtsiz)
+        yield result
+
+
+def dbfpack(dbf, names=None):
+    """Make a ChannelPack from dbf data file.
+
+    Parameters:
+    dbf : str or file
+        If a file it should be opened for binary reads.
+    names : list of str
+        A sequence of names to read. If not provided read all.
 
     """
 
-    if keystyle not in ['ints', 'names']:
-        raise ValueError('Unknown keyword: ' + str(keystyle))
+    if not names:
+        with contextopen(dbf, 'rb') as context:
+            fo = context.fo
+            dbfrecs = dbfreader(fo)
+            fieldnames = next(dbfrecs)
+            next(dbfrecs)       # specs
+            columns = zip(*dbfrecs)
 
-    with open(fn, 'rb') as fo:
-        rit = dbfreader(fo)
-        names = rit.next()
-        specs = rit.next()      # NOQA
-        R = [tuple(r) for r in rit]
+        data = {i: column for i, column in enumerate(columns)}
+        chnames = {i: name for i, name in enumerate(fieldnames)}
+        pack = ChannelPack(data, chnames)
+        pack.fn = context.name
+        return pack
 
-    def getkey(i):
-        if keystyle == 'ints':
-            return i
-        else:
-            return names[i]
+    with contextopen(dbf, 'rb') as context:
+        fo = context.fo
+        sniffnames = dbfreader(fo)
+        allnames = next(sniffnames)
+        fo.seek(0)
+        dbfrecs = dbfrecords(fo, names)
+        next(dbfrecs)
+        next(dbfrecs)
+        columns = zip(*dbfrecs)
 
-    R = zip(*R)
-    d = dict()
-    for i in usecols or range(len(names)):
-        # d[getkey(i)] = R['f' + str(i)]  # Default numpy fieldname
-        d[getkey(i)] = np.array(R[i])
-    return d
+    chnames = {}
+    for i, name in enumerate(allnames):
+        if name in names:
+            chnames[i] = name  # duplicates possible
 
+    data = {i: column for i, column in zip(sorted(chnames), columns)}
+    pack = ChannelPack(data, chnames)
+    pack.fn = context.name
 
-def channel_names(fn, usecols=None):
-    """Return the field names (channel names) from dbf file fn. With
-    usecols, return only names corresponding to the integers in
-    usecols."""
-
-    with open(fn, 'rb') as fo:
-        names = dbfreader(fo).next()
-
-    return usecols and [names[i] for i in usecols] or names
+    return pack
